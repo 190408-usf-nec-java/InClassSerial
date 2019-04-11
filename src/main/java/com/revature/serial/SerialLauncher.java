213e8ce6523b52837036398762b6036acecbf943
unknown <dwightanthony@live.com>
2019-04-11 13:29:05 -0400

    Append to file

--- src/main/java/com/revature/serial/SerialLauncher.java
+++ src/main/java/com/revature/serial/SerialLauncher.java
@@ -36,7 +36,7 @@ public class SerialLauncher {
 		aList.add(new Associate("Breeanna Lane", "IT ", 20, "Leaning Tower of Cheese Pizza", "018-778-9990"));
 		aList.add(new Associate("Peterson Etienne", "Dev ", 28, "Chicken", "018-758-6890"));
 		aList.add(new Associate("Chris Copeland","Revature Associate",22,"CheeseBurger","666-22-4488"));
-
+		aList.add(new Associate("Dwight Brown", "Trainee ", 24, "Jamaican Jerk Fish", "987-65-4321"));
 
 		
 		serializeToFile(fileName, aList);
